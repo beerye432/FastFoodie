c9f2fc253e52669f051f3febf44409cf09cfa6e3
Brian Clark <beerye432@gmail.com>
2016-08-09 19:48:25 -0700

    first commit to git

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+ 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/admin', function(req, res, next){
+	res.render('admin', {title: 'Admin' });
+})
+ 
+router.get('/restaurants/:name', function(req, res, next){
+ 
+    res.render('res-temp');
+});
+
+router.get('/charts', function(req, res, next){
+	res.render('charts');
+});
+
+router.get('/restaurants/:name1/:name2', function(req, res, next){
+
+	res.render('item-temp');
+});
+ 
+router.get('/restaurants', function(req, res, next){
+
+    res.render('restaurants');
+});
+
+router.get('/login', function(req, res, next){
+	res.render('login');
+});
+
+router.get('/profile', function(req, res, next){
+	res.render('profile');
+});
+
+router.get('/profile/:userID', function(req, res, next){
+	res.render('profile-temp');
+});
+ 
+module.exports = router;
